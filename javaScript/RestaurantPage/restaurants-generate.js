6bde5568461f9974b2092c7126449b59156e241e
cathrinejakobsen <72168843+cathrinejakobsen@users.noreply.github.com>
2021-05-24 00:25:23 +0200

    restaurantsPage

--- javaScript/RestaurantPage/restaurants-generate.js
+++ javaScript/RestaurantPage/restaurants-generate.js
@@ -1,21 +1,51 @@
-import RestaurantsModule from './restaurantsModule.js'
+import {RestaurantsModule} from './restaurantsModule.js'
 
 const restaurantsSection = document.querySelector("#restaurants-section");
+const modalContainer = document.querySelector("#modal-container");
 
-let htmlTxt = "";
+let restaurantCardTxt = "";
+
 RestaurantsModule.getAllRestaurants().forEach(restaurants => {
-    htmlTxt += `
-        <article class="column">
-            <div class="card">
-                <section class="card-image">
-                    <img src="../images/restaurants/${restaurants.image}">
-                </section>
-                <section class="card-content">
-                    <h3>${restaurants.restaurant}</h3>
-                </section>
+    restaurantCardTxt += `
+            <!-- RESTAURANT CARDS -->
+            <article class="column">
+                <div class="card">
+                    <section class="card-image">
+                        <img src="../images/restaurants/${restaurants.image}">
+                    </section>
+                    <section class="card-content">
+                        <a class="popup-btn">${restaurants.restaurant}</a>
+                    </section>
+                </div>
+            </article>
+            
+            <!-- MODAL POPUP -->
+            <div class="modal">
+                <div class="modal-background"></div>
+                <div class="modal-card">
+
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">${restaurants.restaurant}</p>
+                        <button class="delete" aria-label="close"></button>
+                    </header>
+
+                    <section class="modal-card-body">
+                        <p>Address: ${restaurants.address}</p>
+                        <p>Leader: ${restaurants.leader}</p>
+                        <p>Phone: ${restaurants.phone}</p>
+                        <p>Mail: ${restaurants.mail}</p>
+                        <p>Opening times: ${restaurants.openingtimes}</p>
+                        <p>Employees: ${restaurants.emplyees}</p>
+                        <p>Established: ${restaurants.established}</p>
+                    </section>
+
+                    <footer class="modal-card-foot">
+                            <button class="button is-success">Save changes</button>
+                            <button class="delete">Cancel</button>
+                    </footer>
+                </div>
             </div>
-        </article>
     `;
 });
 
-restaurantsSection.innerHTML = htmlTxt;
+restaurantsSection.innerHTML = restaurantCardTxt;
