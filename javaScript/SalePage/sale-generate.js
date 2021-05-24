7bc5a9aef02bc5eca7c7570a4c5d38f6085486a0
TonjeHusvik <tonje.husvik@hotmail.com>
2021-05-24 09:01:00 +0200

    fikset drop-down og knapper

--- javaScript/SalePage/sale-generate.js
+++ javaScript/SalePage/sale-generate.js
@@ -19,14 +19,63 @@ saleModule.getAllSales().forEach(sales => {
 
 saleSection.innerHTML = htmlTxt;
 
-// KNAPPER
-var button1 = document.getElementById("button1").innerHTML;
-var button2 = document.getElementById("button2");
+//dropdown Butikk
+const menu = document.getElementById('dropdown');
+menu.addEventListener('click', e=>{
+    e.stopPropagation();
+    menu.classList.add('is-active');
+})
 
-button1.onclick = function(){
-    prompt("omsetning");
-}
+document.addEventListener('click', ()=>{
+    menu.classList.remove('is-active');
+})
+//dropdown År
+const menu1 = document.getElementById('dropdown1');
+menu1.addEventListener('click', e=>{
+    e.stopPropagation();
+    menu1.classList.add('is-active');
+})
 
-button2.onclick = function(){
-    prompt("omsetning - retter her");
-}
+document.addEventListener('click', ()=>{
+    menu1.classList.remove('is-active');
+})
+//dropdown Måned
+const menu2 = document.getElementById('dropdown2');
+menu2.addEventListener('click', e=>{
+    e.stopPropagation();
+    menu2.classList.add('is-active');
+})
+
+document.addEventListener('click', ()=>{
+    menu2.classList.remove('is-active');
+})
+//dropdown Omsetning
+const menu3 = document.getElementById('dropdown3');
+menu3.addEventListener('click', e=>{
+    e.stopPropagation();
+    menu3.classList.add('is-active');
+})
+
+document.addEventListener('click', ()=>{
+    menu3.classList.remove('is-active');
+})
+
+//tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(item => item.classList.remove('is-active'))
+        tab.classList.add('is-active');
+
+        const target = tab.dataset.target;
+        tabContentBoxes.forEach(box => {
+            if (box.getAttribute('id') === target) {
+                box.classList.remove('is-hidden');
+            } else {
+                box.classList.toggle('is-hidden');
+            }
+        });
+    })
+})
