d7e5aff0caaf1b103efa37fe23f44043585ac6b7
cathrinejakobsen <72168843+cathrinejakobsen@users.noreply.github.com>
2021-05-27 00:09:45 +0200

    Restaurants

--- javaScript/RestaurantPage/restaurants-generate.js
+++ javaScript/RestaurantPage/restaurants-generate.js
@@ -1,14 +1,47 @@
 import {RestaurantsModule} from './restaurantsModule.js'
 
+let confirmationMessage = document.getElementsByClassName(".confirmation-message");
+
+// function to push info from user input into restaurants array, to add a new restaurant
+let pushData = () => {
+   // ev.preventDefault(); // to stop the form from submitting    
+    var result = confirm("Legg til restaurant?"); // popup to confirm before you submit
+    if (result) {
+        var input = {};
+        
+        input.restaurant = document.getElementById('name-input').value;
+        input.image = "default.png";
+        input.id = 5;
+        input.address = document.getElementById('address-input').value;
+        input.leader = document.getElementById('leader-input').value;
+        input.phone = document.getElementById('phone-input').value;
+        input.mail = document.getElementById('mail-input').value;
+        input.openingtimes = document.getElementById('opening-times-input').value;
+        input.employees = document.getElementById('employees-input').value;
+        input.established = document.getElementById('established-input').value
+                
+        RestaurantsModule.restaurants.push(input);
+        confirmationMessage.innerHTML = "Restauranten har blitt lagt til!";
+        document.querySelector('form').reset(); // reset form after submitting
+    }
+}
+
+// confirmation message before deleting a restaurant, followed by a message confirming the removal
+let editRestaurantOK = () => {
+    var result = confirm("Vil du virkelig slette denne restauranten?"); // popup to confirm before you submit
+    if (result) {
+        confirmationMessage.innerHTML = "Restauranten er slettet!";
+    }
+}
+
 // getting all html-elements
 const restaurantsSection = document.querySelector("#restaurants-section");
-const editRestaurantsModal = document.querySelector("#edit-restaurants-modal");
 const deleteRestaurantsModal = document.querySelector("#delete-restaurants-modal");
 const addRestaurantBtn = document.querySelector("#add-restaurant-btn");
+const deleteRestaurantBtn = document.querySelector(".delete-restaurant-btn");
 
 // empty variables
 let restaurantCardTxt = "";
-let editRestaurantsModalTxt = "";
 let deleteRestaurantsModalTxt = "";
 
 // looping through and printing out all restaurants with their modals
@@ -21,7 +54,7 @@ RestaurantsModule.getAllRestaurants().forEach(restaurants => {
                     <img src="../images/restaurants/${restaurants.image}">
                 </section>
                 <section class="card-content">
-                    <a class="popup-btn">${restaurants.restaurant}</a>
+                    <p class="popup-btn restaurant-card-name">${restaurants.restaurant}</p>
                 </section>
             </div>
         </article>
@@ -32,34 +65,59 @@ RestaurantsModule.getAllRestaurants().forEach(restaurants => {
             <div class="modal-card">
 
                 <header class="modal-card-head">
-                    <p class="modal-card-title">${restaurants.restaurant}</p>
+                    <div class="modal-card-title">
+                    <p class="modal-card-title-main">${restaurants.restaurant}</p>
+                    <p class="modal-card-title-second">${restaurants.address}</p></div>
                     <button class="delete" aria-label="close"></button>
                 </header>
 
                 <section class="modal-card-body">
-                    <p>Address: ${restaurants.address}</p>
-                    <p>Leader: ${restaurants.leader}</p>
-                    <p>Phone: ${restaurants.phone}</p>
-                    <p>Mail: ${restaurants.mail}</p>
-                    <p>Opening times: ${restaurants.openingtimes}</p>
-                    <p>Employees: ${restaurants.emplyees}</p>
-                    <p>Established: ${restaurants.established}</p>
+                    <div class="columns">
+                        <div class="column is-6">
+                            <p><b>Leder</b><br>${restaurants.leader}</p><br>
+                            <p><b>Telefon</b></p><p>${restaurants.phone}</p><br>
+                            <p><b>E-post</b></p><p>${restaurants.mail}</p>
+                        </div>
+                        <div class="column is-6">
+                            <p><b>Åpningstider</b></p><p>${restaurants.openingtimes}</p><br>
+                            <p><b>Ansatte</b></p><p>${restaurants.emplyees}</p><br>
+                            <p><b>Etablert</b></p><p>${restaurants.established}</p>
+                        </div>
+                    </div>
+                    <div class="buttons">
+                        <a class="button is-warning is-small">
+                            <span class="icon is-small">
+                                <i class="fas fa-coins"></i>
+                            </span>
+                            <span>Omsetning</span>
+                        </a>
+                        <a class="button is-warning is-small">
+                            <span class="icon is-small">
+                                <i class="fas fa-user-friends"></i>
+                            </span>
+                            <span>Ansatte</span>
+                        </a>
+                        <a class="button is-warning is-small">
+                            <span class="icon is-small">
+                                <i class="fas fa-utensils"></i>
+                            </span>
+                            <span>Meny</span>
+                        </a>
+                    </div>
                 </section>
 
+
                 <footer class="modal-card-foot">
-                        <button class="button is-success">Save changes</button>
-                        <button class="button modal-cancel-btn">Cancel</button>
+                        <button class="button modal-cancel-btn">Avslutt</button>
+                        <button class="button is-light is-small edit-button">
+                            <span class="icon is-small">
+                                <i class="fas fa-pencil-alt"></i>
+                            </span>
+                            <span>Rediger</span>
+                        </button>
                 </footer>
             </div>
         </div>
-    `;
-});
-
-// printing out all restaurant names so you can choose which one to edit
-RestaurantsModule.getAllRestaurants().forEach(restaurants => {
-    editRestaurantsModalTxt += `
-        <input type="checkbox" class="restaurant${restaurants.id}" name="restaurant${restaurants.id}" value="${restaurants.id}">
-        <label for="restaurant${restaurants.id}">${restaurants.restaurant}</label><br>
     `;
 });
 
@@ -72,28 +130,60 @@ RestaurantsModule.getAllRestaurants().forEach(restaurants => {
 });
 
 // function for user to add a new restaurant to the array
-const pushData = () =>{
-    var result = confirm("Want to delete?");
+/*
+const pushData = (ev) =>{
+    ev.preventDefault(); // to stop the form from submitting
+
+    // popup to confirm before you submit
+    var result = confirm("Legg til restaurant?"); 
     if (result) {
-        const nameInput = document.getElementById('name-input').value;
-        const addressInput = document.getElementById('address-input').value;
-        const leaderInput = document.getElementById('leader-input').value;
-        const phoneInput = document.getElementById('phone-input').value;
-        const mailInput = document.getElementById('mail-input').value;
-        const openingTimesInput = document.getElementById('opening-times-input').value;
-        const employeesInput = document.getElementById('employees-input').value;
-        const establishedInput = document.getElementById('established-input').valuue;
-        
-        RestaurantsModule.restaurants.push(`"restaurant: ${nameInput}", "image: ${addressInput}", "id: 5", "address: ${addressInput}", "leader: ${leaderInput}", "phone: ${phoneInput}", "mail: ${mailInput}", "openingtimes: ${openingTimesInput}", "emplyees: ${employeesInput}", "established: ${establishedInput}"`)
+        let array = {
+            restaurant: document.getElementById('name-input').value,
+            image: "default.png",
+            id: 5,
+            address: document.getElementById('address-input').value,
+            leader: document.getElementById('leader-input').value,
+            phone: document.getElementById('phone-input').value,
+            mail: document.getElementById('mail-input').value,
+            openingtimes: document.getElementById('opening-times-input').value,
+            employees: document.getElementById('employees-input').value,
+            established: document.getElementById('established-input').value
+        }
+                
+        RestaurantsModule.restaurants.push(input);
+        document.querySelector('form').reset(); // reset form after submitting
     }
 }
+*/
+/*
+    let data_array = [];
+    
+    let my_object = {}; 
 
+    my_object.name = "stack";
+    my_object.age = 20;
+    my_object.hair_color = "red";
+    my_object.eye_color = "green";
+    
+    data_array.push(my_object);
+
+var array = [];
+var obj = {};
+obj["01"] = array.label;
+obj["02"] = array.value;
+array.push(obj);
+*/
 
 restaurantsSection.innerHTML = restaurantCardTxt;
-editRestaurantsModal.innerHTML = editRestaurantsModalTxt;
 deleteRestaurantsModal.innerHTML = deleteRestaurantsModalTxt;
 
-
+// to make sure the document is loaded before function runs
 document.addEventListener('DOMContentLoaded', () => {
     addRestaurantBtn.addEventListener('click', pushData);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    deleteRestaurantBtn.addEventListener('click', editRestaurantOK);
+});
+
+
--- javaScript/RestaurantPage/restaurantsModule.js
+++ javaScript/RestaurantPage/restaurantsModule.js
@@ -1,15 +1,18 @@
+export let RestaurantsModule = (function(){ // IIFE
 
-let restaurants = [
-    {restaurant: "Oslo S", image: "oslos-restaurant.png", id: 1, address: "Oslo Svinger 34", leader: "Silje Antonsen", phone: "624 95 345", mail: "oslos@gyldnepizza.no", openingtimes: "Alle dager, 10:00 - 21:00", emplyees: "2 ansatte", established: "2019"},
-    {restaurant: "Majorstua", image: "majorstua-restaurant.png", id: 2, address: "Majorstua 56", leader: "Hanne Davidsen", phone: "638 98 474", mail: "majorstua@gyldnepizza.no", openingtimes: "Alle dager, 11:00 - 22:00", emplyees: "2 ansatte", established: "2019"},
-    {restaurant: "Torshov", image: "torshov-restaurant.png", id: 3, address: "Torresen 90", leader: "Jacob Salg", phone: "678 87 476", mail: "torshov@gyldnepizza.no", openingtimes: "man - fre: 10 - 20, lør - søn: 09:00 - 23:00", emplyees: "2 ansatte", established: "2019"},
-    {restaurant: "Karl Johan", image: "karljohan-restaurant.png", id: 4, address: "Karl Johans Gate 87", leader: "Sonja Finne", phone: "765 37 384", mail: "karljohan@gyldnepizza.no", openingtimes: "man - fre: 09:00 - 21:00, lør - søn: 08:00 - 24:00", emplyees: "2 ansatte", established: "2019"}
-];
+    let restaurants = [
+        {restaurant: "Oslo S", image: "oslos-restaurant.png", id: 1, address: "Oslo Svinger 34, 0342 Oslo", leader: "Silje Antonsen", phone: "624 95 345", mail: "oslos@gyldnepizza.no", openingtimes: "Alle dager, 10:00 - 21:00", emplyees: "2 ansatte", established: "2019"},
+        {restaurant: "Majorstua", image: "majorstua-restaurant.png", id: 2, address: "Majorstua 56, 0468 Oslo", leader: "Hanne Davidsen", phone: "638 98 474", mail: "majorstua@gyldnepizza.no", openingtimes: "Alle dager, 11:00 - 22:00", emplyees: "2 ansatte", established: "2019"},
+        {restaurant: "Torshov", image: "torshov-restaurant.png", id: 3, address: "Torresen 90, 0360 Oslo", leader: "Jacob Salg", phone: "678 87 476", mail: "torshov@gyldnepizza.no", openingtimes: "man - fre: 10 - 20, lør - søn: 09:00 - 23:00", emplyees: "2 ansatte", established: "2019"},
+        {restaurant: "Karl Johan", image: "karljohan-restaurant.png", id: 4, address: "Karl Johans Gate 87, 0908 Oslo", leader: "Sonja Finne", phone: "765 37 384", mail: "karljohan@gyldnepizza.no", openingtimes: "man - fre: 09:00 - 21:00, lør - søn: 08:00 - 24:00", emplyees: "2 ansatte", established: "2019"}
+    ];
 
-export const RestaurantsModule = (function(){ // IIFE
+    let getAllRestaurants = () => restaurants;
 
-    const getAllRestaurants = () => restaurants;
+    return {getAllRestaurants, restaurants}
 
-    return {getAllRestaurants}
 
 }()); // end of RestaurantsModule
+
+        
+ 
